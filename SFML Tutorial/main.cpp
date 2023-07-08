20263e2ce55f862b64d99e824d3aade5560c7c8e
dylanjhall <dylanjhall@gmail.com>
2023-07-08 03:27:08 -0400

    added player and player movement

--- SFML Tutorial/main.cpp
+++ SFML Tutorial/main.cpp
@@ -5,6 +5,8 @@ int main()
 {
     sf::RenderWindow window(sf::VideoMode(512, 512), "SFML Tutorial", sf::Style::Close | sf::Style::Resize);
     sf::CircleShape shape(100.f);
+    sf::RectangleShape player(sf::Vector2f(100.f, 100.f));
+    player.setFillColor(sf::Color::Red);
     shape.setFillColor(sf::Color::Green);
 
     while (window.isOpen())
@@ -15,8 +17,8 @@ int main()
             switch (event.type)
             {
             case sf::Event::Closed:
-                    window.close();
-                    break;
+                window.close();
+                break;
             case sf::Event::Resized:
 
                 //std::cout << "New window width:" << event.size.width << " New window height:" << event.size.height << std::endl;
@@ -25,15 +27,41 @@ int main()
             case sf::Event::TextEntered:
             {
                 if (event.text.unicode < 128)
-                    printf("%c", event.text.unicode);
-            }
 
+                    printf("%c", event.text.unicode);
+
+
+            }
+            case sf::Event::KeyPressed:
+            {
+                if (event.key.code == sf::Keyboard::Enter)
+
+                    printf("\n");
+                if (event.key.code == sf::Keyboard::Delete)
+                    window.close();
+            }
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
+            {
+                player.move(-1.f, 0.f);
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
+            {
+                player.move(1.f, 0.f);
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
+            {
+                player.move(0.f, -1.f);
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
+            {
+                player.move(0.0f, 1.f);
             }
            
         }
 
         window.clear();
-        window.draw(shape);
+        window.draw(player);
         window.display();
     }
 
